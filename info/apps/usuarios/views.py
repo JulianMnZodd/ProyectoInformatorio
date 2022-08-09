1d301c558ed936481dd8bccd82745205fefec449
Navarro Mauricio <julianmn15@gmail.com>
2022-08-09 15:16:37 -0300

    Actualizando la base y el home del proyecto

--- info/apps/usuarios/views.py
+++ info/apps/usuarios/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def Logearse(request):
+    return render(request,'usuarios/Logearse.html')
+
+def Registrarse(request):
+    return render(request,'usuarios/Registrarse.html')
