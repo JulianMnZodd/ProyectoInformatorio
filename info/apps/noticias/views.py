8f67fc0cc28d487113f7a6ed127243f8fdc085ec
Navarro Mauricio <julianmn15@gmail.com>
2022-08-10 19:37:37 -0300

    Actualizando la ventana noticias

--- info/apps/noticias/views.py
+++ info/apps/noticias/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-
+from .models import Noticia
 # Create your views here.
 
 def Listar(request):
-    return render(request,'noticias/listar_noticias.html')
+    #Creo el diccionario para pasar datos al templeate
+    ctx={}
+    #Buscar las noticias de la BD
+    todas=Noticia.objects.all()
+    #Buscar lo que quiera de la BD
+    ctx['notis']=todas
+    
+    return render(request,'noticias/listar_noticias.html',ctx)
+#EJEMPLO DE COMO DESARMA EL CTX EL TEMPLETA
+# ctx['nombre']='nicolas'
