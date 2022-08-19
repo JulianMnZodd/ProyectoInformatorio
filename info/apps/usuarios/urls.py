6329969a56512a6b20e713e750f0a18029d1098d
Ernesto <efcoro@gmail.com>
2022-08-19 17:35:20 -0300

    login rapido

--- info/apps/usuarios/urls.py
+++ info/apps/usuarios/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from . import views
 
+from django.contrib.auth import views as auth
+
 app_name= 'usuarios'
 
 urlpatterns = [
-    path('ingresar/',views.Logearse, name='Logearse'),
+    
     path('registrarse/',views.Registrarse, name='Registrarse'),
     path('index/',views.index, name='index'),
+    path('login/',auth.LoginView.as_view(template_name='usuarios/Logearse.html'),name='Logearse'),
+    path('logout/',auth.LogoutView.as_view(),name="logout"),
+    
 ]
--- info/info/urls.py
+++ info/info/urls.py
@@ -23,12 +23,14 @@ from django.conf import settings
 from . import views
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.Home, name = 'home'),
     path('contacto/', views.contacto, name = 'contacto'),
     path('noticias/',include('apps.noticias.urls')),
     path('usuarios/',include('apps.usuarios.urls')),
+
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
