ca3222f5b3fd0e849359ada53782ec529154dfa8
Navarro Mauricio <julianmn15@gmail.com>
2022-08-19 13:59:13 -0300

    Agregando las noticias nuevas en el home

--- info/info/views.py
+++ info/info/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+from apps.noticias.models import Noticia
+
 def Home(request):
-    return render(request,'home.html')
+    ctx={}
+    noticia=Noticia.objects.all().order_by('-creado')[:4]
+    ctx['noticia']=noticia
+    return render(request,'home.html',ctx)
+    
 
 def contacto(request):
     return render(request,'contacto.html')
