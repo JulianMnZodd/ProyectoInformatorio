266dc5fdd0ec6d152e045b05ffcbf85c83f567d9
Rodrigoocordoba <rodrigoojcordoba@gmail.com>
2022-08-21 01:05:24 -0300

    models de actividades creado y actividades cargadas

--- info/apps/actividades/views.py
+++ info/apps/actividades/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+from . models import Actividad
 # Create your views here.
 
 def Acts(request):
-    return render(request,'actividades/listar_actividades.html')
+
+    ctx = {} 
+    todas = Actividad.objects.all()
+    print(todas) 
+    ctx['acts'] = todas
+
+    return render(request,'actividades/listar_actividades.html',ctx)
--- info/info/settings/base.py
+++ info/info/settings/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.usuarios',#cargo la nueva app
     'apps.noticias',
+    'apps.actividades',
 ]
 
 MIDDLEWARE = [
