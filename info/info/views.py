a665718e552c902fe885bdb594dbe93187722e8a
Navarro Mauricio <julianmn15@gmail.com>
2022-08-27 19:20:25 -0300

    agregando logo

--- info/info/views.py
+++ info/info/views.py
@@ -9,20 +9,6 @@ def Home(request):
     actividad=Actividad.objects.all().order_by('-fecha')[:3]
     return render(request,'home.html',{'noticia':noticia,'actividad':actividad})
 
-'''
-def Home(request):
-<<<<<<< HEAD
-    noticia=Noticia.objects.all().order_by('-creado')[:3]
-    actividad=Actividad.objects.all().order_by('-fecha')[:3]
-    return render(request,'home.html',{'noticia':noticia,'actividad':actividad})
-    
-=======
-    ctx={}
-    noticia=Noticia.objects.all().order_by('-creado')[:4]
-    ctx['noticia']=noticia
-    return render(request,'home.html',ctx)    
-'''
->>>>>>> b0243a487fe79dd3fdb8ab51cd23feb3057fb85c
 
 def contacto(request):
     return render(request,'contacto.html')
