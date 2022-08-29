9065d332c99576c90a240550af06914367930af5
Rodrigoocordoba <rodrigoojcordoba@gmail.com>
2022-08-29 20:03:57 -0300

    Agregando hora en BD de actividades

--- info/apps/actividades/models.py
+++ info/apps/actividades/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Animador(models.Model):
     nombre = models.CharField(max_length = 60) 
-    zona = models.CharField(max_length = 60) 
+    zona = models.CharField(max_length = 60, null= True, blank = True) 
 
     def __str__(self):
         return self.nombre
@@ -13,7 +13,7 @@ class Animador(models.Model):
 class Actividad(models.Model):
     titulo = models.CharField(max_length=100)
     cuerpo = models.TextField()
-    fecha = models.DateField()
+    fecha = models.DateTimeField()
     imagen = models.ImageField(upload_to = 'actividades', null = True, blank=True)
     animador = models.ForeignKey(Animador, on_delete = models.CASCADE, null = True)
 
